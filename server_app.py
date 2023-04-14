bea1b93b7d2b963bb2604d279b561bbbb2939671
jpcreis15 <joaoreis.correia@gmail.com>
2023-04-14 21:07:52 +0100

    fixed sever app

--- server_app.py
+++ server_app.py
@@ -1,7 +1,6 @@
 import pickle
-from sklearn.tree import DecisionTreeClassifier
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, File, UploadFile, Request, Form
+from fastapi import FastAPI
 import json
 
 # Opening the file might also be once the inference endpoint is called
@@ -19,16 +18,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Health check service
 @app.get("/healthcheck")
 async def health():
+    """
+    Health Check service
+    """
+
     return {"message":"all good"}
 
+# Inference service
 @app.post("/inference")
 async def predict(input: list):
+    """
+    Inference Service
+    """
+
     features = list(input)
-    
     print("features: ", features)
 
     pred = model.predict([features])
     print("prediction: ", pred)
+
     return json.dumps({"result": pred[0]})
